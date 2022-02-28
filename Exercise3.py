43662c1f0c521b541c6cc62f62a9ea6c725694c1
BartvanWoesik <bartwoesik1@gmail.com>
2022-02-28 11:05:17 +0100

    update

--- Exercise3.py
+++ Exercise3.py
@@ -1,7 +1,9 @@
 
+from multiprocessing import pool
 import numpy as np
-from PIL import Image, ImageOps
+
 import matplotlib.pyplot as plt
+from matplotlib import image
 
 from tensorflow import keras
 
@@ -10,13 +12,10 @@ import math
 (x_train, y_train), (x_test, y_test) =keras.datasets.cifar10.load_data()
 x_train, x_test = x_train /255, x_test/255
 
-img = x_train[4]
-
-img = np.array(img)
+img = image.imread("test2.jpg")
 
 
-
-
+print(img.shape)
 
 plt.imshow(img)
 plt.show()
@@ -24,61 +23,94 @@ plt.show()
 
 
 kernel1 = np.array( [[-1,-1,-1],
-                    [0,0,0,],
-                    [1,1,1,]],
+                    [-1,8,-1],
+                    [-1,-1,-1]],
+                   )
+kernel2 = np.array( [[-1,0,1],
+                    [-2,0,2],
+                    [-1,0,1]],
                    )
 
 
 
-def calculate_target_size(img_size: int, kernel_size: int) -> int:
-    num_pixels = 0
+
+
+def convolve2(img: np.array, kernel: np.array) -> np.array:
+
+    kernel_height   = kernel.shape[0] 
+    kernel_width    = kernel.shape[1]
+    img_height      = img.shape[0]
+    img_width       = img.shape[1]
+
+    # find out the colors of the image
+    try:
+        img_depth       = img.shape[2]
+    except:
+        img_depth = 1
+
     
-    # From 0 up to img size (if img size = 224, then up to 223)
-    for i in range(img_size):
-        # Add the kernel size (let's say 3) to the current i
-        added = i + kernel_size
-        # It must be lower than the image size
-        if added <= img_size:
-            # Increment if so
-            num_pixels += 1
-            
-    return num_pixels
+    mis_match_width = math.floor(kernel_width/2)
+    mis_match_height = math.floor(kernel_height/2)
 
+    fit_width = img_width - 2*mis_match_width
+    fit_height= img_height - 2*mis_match_height
 
+    new_img = np.zeros(shape=( fit_height,fit_width, img_depth))
+    print(new_img.shape)
 
-
-
-def convolve(img: np.array, kernel: np.array) -> np.array:
-    # Assuming a rectangular image
-    tgt_size = calculate_target_size(
-        img_size=img.shape[0],
-        kernel_size=kernel.shape[0]
-    )
-    # To simplify things
-    k = kernel.shape[0]
-    
-    # 2D array of zeros
-    convolved_img = np.zeros(shape=(tgt_size, tgt_size))
-    
-    # Iterate over the rows
-    for i in range(tgt_size):
+    for x in range(0, img_depth):
+        for j in range( fit_width- mis_match_width):
         # Iterate over the columns
-        for j in range(tgt_size):
-            # img[i, j] = individual pixel value
-            # Get the current matrix
-            mat = img[i:i+k, j:j+k]
-            
-            # Apply the convolution - element-wise multiplication and summation of the result
-            # Store the result to i-th row and j-th column of our convolved_img array
-            convolved_img[i, j] = np.sum(np.multiply(mat, kernel))
-            
-    return convolved_img
+            for i in range( fit_height - mis_match_height):
+                # img[i, j] = individual pixel value
+                # Get the current matrix
+                mat = img[i:i+kernel_width, j:j+kernel_height, x]
+                
+                # Apply the convolution - element-wise multiplication and summation of the result
+                # Store the result to i-th row and j-th column of our convolved_img array
+                new_img[i+mis_match_height, j+ mis_match_width,x] = np.sum(np.multiply(mat, kernel))
 
-img2 = np.zeros(shape=(30,30,3))
-for i in range(img.shape[2]):
+    return new_img
 
-    img2[:,:,i] = convolve(img[:,:,i], kernel1)
-plt.imshow(img2)
+
+new_img = convolve2(img, kernel2)
+
+
+
+def rel(img: np.array) -> np.array:
+    new_img = img
+    new_img[new_img <0] = 0
+    return new_img
+
+def max_pooling(img: np.array, pool_height, pool_width) -> np.array:
+    img_width = img.shape[1]
+    img_height = img.shape[0]
+    # find out the colors of the image
+    try:
+        img_depth       = img.shape[2]
+    except:
+        img_depth = 1
+
+    fit_width = int(math.floor(img_width / pool_width))
+    fit_height = int(math.floor(img_height / pool_height))
+
+
+    # Find the dimensions of the boundries where the pooling size does not fit
+    left_widtch = img_width % pool_width
+    left_height = img_height % pool_height
+
+
+    # Go through all whole pooling fit areas of the image
+    for x in range(img_dept):
+        for j in range(fit_width):
+            for i in range(fit_height):
+                pass
+    
+
+
+
+new_img = rel(new_img)
+new_img= max_pooling(new_img[:,:,1],3,3)
+plt.imshow(new_img)
 plt.show()
-
-
+print(new_img)
